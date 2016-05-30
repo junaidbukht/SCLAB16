11273767738041e18670445a08098a391bac852a
junaid <junaidbukht@gmail.com>
2016-05-30 17:13:30 +0500

    abcd

---
+++ src/Add.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Add {
+	
+	int a,b,sum;
+	
+	public void sum1()
+	{
+		Scanner ss= new Scanner(System.in);
+		System.out.println("Write First Number");
+		a=ss.nextInt();
+		System.out.println("Write Second Number");
+		b=ss.nextInt();
+		sum=a+b;
+		System.out.println("Sum is" + sum);
+	}
+
+}
---
+++ src/Caculate.java
@@ -0,0 +1,11 @@
+
+public class Caculate {
+
+	public static void main(String[] args)
+
+	{
+		Add ad=new Add();
+		ad.sum1();
+		
+	}
+}
